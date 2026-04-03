085f69018b4f11e3124d5ed6afc205bee71985d7
Uday <uday.kurien@gmail.com>
2026-04-02 23:36:06 -0400

    Fix typo in ln -s command for hyprland dots.

--- scripts_to_manage_dots/copy_dots_to_correct_dirs.sh
+++ scripts_to_manage_dots/copy_dots_to_correct_dirs.sh
@@ -6,8 +6,8 @@ ln -s ~/Stuff/Private/Dots_GitHub/niri/config.kdl /home/echoes/.config/niri/
 
 # hyprland
 mkdir -p /home/echoes/.config/hypr
-ln -s ln -s ~/Stuff/Private/Dots_GitHub/hyprland/hyprland.conf /home/echoes/.config/hypr/
-ln -s ln -s ~/Stuff/Private/Dots_GitHub/hyprland/hyprpaper.conf /home/echoes/.config/hypr/
+ln -s ~/Stuff/Private/Dots_GitHub/hyprland/hyprland.conf /home/echoes/.config/hypr/
+ln -s ~/Stuff/Private/Dots_GitHub/hyprland/hyprpaper.conf /home/echoes/.config/hypr/
 
 # zshrc
 ln -s ~/Stuff/Private/Dots_GitHub/zsh/zshrc_main ~/.zshrc
