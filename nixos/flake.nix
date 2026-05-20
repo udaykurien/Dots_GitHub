dca7a066a010f6b91d416ee2219f5572587ae17e
uk <uday.kurien@gmail.com>
2026-05-20 08:26:23 -0400

    Update nix config.

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -6,9 +6,9 @@
     nix-flatpak.url = "github:gmodena/nix-flatpak";
   };
   outputs = { self, nixpkgs, nixpkgs-unstable, nix-flatpak, ... }: {
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
+    nixosConfigurations.SpiritBox = nixpkgs.lib.nixosSystem {
       modules = [
+       {nixpkgs.hostPlatform = "x86_64-linux";}
         ./configuration.nix
         nix-flatpak.nixosModules.nix-flatpak
         {
