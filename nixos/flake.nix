4d16ca87334c66feb28877c5fca4455e53379285
uk <uday.kurien@gmail.com>
2026-05-25 14:31:29 -0400

    Some updates.

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -5,19 +5,17 @@
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     nix-flatpak.url = "github:gmodena/nix-flatpak";
   };
-  outputs = { self, nixpkgs, nixpkgs-unstable, nix-flatpak, ... }: {
+  outputs = { self, nixpkgs, nixpkgs-unstable, nix-flatpak, ... }:
+  let
+    system = "x86_64-linux";
+    pkgs-unstable = nixpkgs-unstable.legacyPackages.${system};
+  in {
     nixosConfigurations.SpiritBox = nixpkgs.lib.nixosSystem {
+      inherit system;
+      specialArgs = { inherit pkgs-unstable; };
       modules = [
-       {nixpkgs.hostPlatform = "x86_64-linux";}
         ./configuration.nix
         nix-flatpak.nixosModules.nix-flatpak
-        {
-          nixpkgs.overlays = [
-            (final: prev: {
-              unstable = nixpkgs-unstable.legacyPackages.${prev.system};
-            })
-          ];
-        }
       ];
     };
   };
