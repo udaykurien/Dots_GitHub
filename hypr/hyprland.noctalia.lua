8d994bd3cd38e274e0678ab87ce27b77ed4274d0
uk <uday.kurien@gmail.com>
2026-07-14 09:29:30 -0400

    Prioritze integrated graphics card for hyprland rendering.

--- hypr/hyprland.noctalia.lua
+++ hypr/hyprland.noctalia.lua
@@ -11,6 +11,8 @@ hl.env("XCURSOR_SIZE", "24")
 hl.env("QT_QPA_PLATFORM", "wayland")
 hl.env("QT_AUTO_SCREEN_SCALE_FACTOR", "1")
 hl.env("MOZ_ENABLE_WAYLAND", "1")
+-- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Multi-GPU/
+hl.env("AQ_DRM_DEVICES", "/dev/dri/card0:/dev/dri/card1")
 
 ------------------
 ---- MONITORS ----
