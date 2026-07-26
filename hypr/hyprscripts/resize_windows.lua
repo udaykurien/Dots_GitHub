44789e96e64062f21b4f6b7c881ad400476df99f
uk <uday.kurien@gmail.com>
2026-07-26 18:41:14 -0400

    Hyprland: Check if window is selected before resizing submap. Change color of border of windows to be resized.

--- hypr/hyprscripts/resize_windows.lua
+++ hypr/hyprscripts/resize_windows.lua
@@ -4,8 +4,29 @@
 local cfg = require("config")
 local main_mod = cfg.main_mod
 
+-- Set rules to highlighted selected windows
+
+local window_to_resize = nil
+local notification_on_time = 2000
+
+hl.window_rule({
+    match = { tag = "marked_for_resize" },
+    border_color = "rgb(FF00AA) rgb(FF00AA)",
+    border_size = 2,
+})
+
 -- Switch to a submap called `resize`.
-hl.bind(main_mod .. " + R", hl.dsp.submap("resize"))
+hl.bind(main_mod .. " + R", function()
+    local win = hl.get_active_window()
+    if not win or not win.address then
+        hl.notification.create({ text = "No window selected.", timeout = notification_on_time})
+        return
+    end
+    window_to_resize = win.address
+    hl.dispatch(hl.dsp.window.tag({ tag = "+marked_for_resize", window = "address:" .. window_to_resize }))
+    hl.notification.create({ text = "Marked for resize submap: " .. win.address, timeout = notification_on_time})
+    hl.dispatch(hl.dsp.submap("resize"))
+end)
 
 -- Start a submap called "resize".
 hl.define_submap("resize", function()
@@ -25,8 +46,20 @@ hl.define_submap("resize", function()
     hl.bind("SHIFT + down", hl.dsp.window.resize({ x = 0, y = 10, relative = true}), { repeating = true })
 
     -- Use `reset` to go back to the global submap
-    hl.bind("escape", hl.dsp.submap("reset"))
-    hl.bind(main_mod .. " + R", hl.dsp.submap("reset"))
+    local function unmark_resize()
+        hl.dispatch(hl.dsp.window.tag({ tag = "-marked_for_resize", window = "address:" .. window_to_resize }))
+        window_to_resize = nil
+    end
+
+    hl.bind("escape", function()
+        unmark_resize()
+        hl.dispatch(hl.dsp.submap("reset"))
+    end)
+
+    hl.bind(main_mod .. " + R", function()
+        unmark_resize()
+        hl.dispatch(hl.dsp.submap("reset"))
+    end)
 
 end)
 
