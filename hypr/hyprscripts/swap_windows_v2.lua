43f0fcec9ec6c75d24b15294178fd32b7faaf8db
uk <uday.kurien@gmail.com>
2026-07-26 09:43:06 -0400

    Hyprland: Change tag of marked windows to be swapped to marked_for_swap.

--- hypr/hyprscripts/swap_windows_v2.lua
+++ hypr/hyprscripts/swap_windows_v2.lua
@@ -14,7 +14,7 @@ local notification_on_time = 2000
 
 -- Set rules to highlighted selected windows
 hl.window_rule({
-    match = { tag = "marked" },
+    match = { tag = "marked_for_swap" },
     border_color = "rgb(AEFF00) rgb(AEFF00)",
     border_size = 2,
 })
@@ -25,8 +25,8 @@ hl.bind(main_mod .. " + m", function()
         local win = hl.get_active_window()
         if win and win.address then
             window_to_swap = win.address
-            hl.dispatch(hl.dsp.window.tag({ tag = "+marked", window = "address:" .. window_to_swap }))
-            hl.notification.create({ text = "Marked:\n" .. window_to_swap, timeout = notification_on_time })
+            hl.dispatch(hl.dsp.window.tag({ tag = "+marked_for_swap", window = "address:" .. window_to_swap }))
+            hl.notification.create({ text = "marked_for_swap:\n" .. window_to_swap, timeout = notification_on_time })
             -- hl.exec_cmd("notify-send -t 2000 'Swap Windows' 'Window markded: '" .. win.address)
         else
             hl.notification.create({ text = "Window or address not found", timeout = notification_on_time })
@@ -34,13 +34,13 @@ hl.bind(main_mod .. " + m", function()
     else
         hl.dispatch(hl.dsp.window.swap({ target = "address:" .. window_to_swap }))
         hl.dispatch(hl.dsp.focus({ window = "address:" .. window_to_swap }))
-        hl.dispatch(hl.dsp.window.tag({ tag = "-marked", window = "address:" .. window_to_swap }))
+        hl.dispatch(hl.dsp.window.tag({ tag = "-marked_for_swap", window = "address:" .. window_to_swap }))
         hl.notification.create({ text = "Swapped:\n" .. window_to_swap, timeout = notification_on_time })
         window_to_swap = nil
     end
 end)
 
--- Handle marked windows being closer
+-- Handle marked_for_swap windows being closer
 hl.on("window.close", function(win)
     if win and win.address == window_to_swap then
         window_to_swap = nil
